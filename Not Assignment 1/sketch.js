3a13b6f5b2b9087d4ef094348ed2de4261fa13c1
Tar626 <114016175+Tar6269@users.noreply.github.com>
2022-09-21 12:15:58 -0600

    Update sketch.js

--- Not Assignment 1/sketch.js
+++ Not Assignment 1/sketch.js
@@ -9,18 +9,8 @@ let black, blackStart;
 let whitePieces, blackPieces;
 let pawn, knight, bishop, rook, queen, king;
 let pieceOne
-
-class Pawn{
-  constructor(location) {
-    // this.team = team;
-    this.location = location;
-    this.shape = circle(location[0], location[1], size/8);
-  }
-  move(){
-    this.location = getMouseTile();
-    
-  }
-}
+let whitePawnOne, whitePawnTwo, whitePawnThree, whitePawnFour, whitePawnFive, whitePawnSix, whitePawnSeven, whitePawnEight;
+let blackPawnOne, blackPawnTwo, blackPawnThree, blackPawnFour, blackPawnFive, blackPawnSix, blackPawnSeven, blackPawnEight;
 function getMouseTile(){
   let tileX = round(map(mouseX, 0, width, 0, 8));
   let tileY = round(map(mouseY, 0, height, 0, 8));
@@ -28,17 +18,41 @@ function getMouseTile(){
   return (tileX, tileY);
 }
 function tileToXY(tileWidth, tileHeight){
-  let tileX = round(map(tileWidth, 0, width, 0, 8));
-  let tileY = round(map(tileHeight, 0, height, 0, 8));
+  let tileX = map(tileWidth -0.5, 0, 8, 0, width);
+  let tileY = map(tileHeight -0.5, 0, 8, 0, height);
 
-  return (tileX, tileY);
+  return [tileX, tileY];
 }
 function setup() {
+  class Pawn{
+    constructor(location) {
+      // this.team = team;
+      this.location = location;
+      // this.shape = circle(location[0], location[1], size/8);
+    }
+    move(){
+      this.location = getMouseTile();
+    }
+    draw(){
+      circle(this.location[0], this.location[1], size/8.5);
+    }
+  }
   size = Math.min(windowWidth, windowHeight)
   createCanvas(size, size);
   black = false;
   blackStart = false;
-  pieceOne = new Pawn(tileToXY(1, 1));
+  whitePieces = [whitePawnOne, whitePawnTwo, whitePawnThree, whitePawnFour, whitePawnFive, whitePawnSix, whitePawnSeven, whitePawnEight];
+  blackPieces = [blackPawnOne, blackPawnTwo, blackPawnThree, blackPawnFour, blackPawnFive, blackPawnSix, blackPawnSeven, blackPawnEight];
+  print(tileToXY(1,1))
+
+  for (let pawn = 0; pawn < whitePieces.length; pawn++) {
+    const element = whitePieces[pawn];
+    whitePieces[pawn] = new Pawn(tileToXY(pawn+1, 2));
+  }
+  for (let pawn = 0; pawn < blackPieces.length; pawn++) {
+    const element = blackPieces[pawn];
+    blackPieces[pawn] = new Pawn(tileToXY(pawn+1, 7));
+  }
   
 }
 
@@ -82,8 +96,21 @@ function drawPieces(){
 function draw() {
 
   drawBoard();
-  print(pieceOne.location);
-  pieceOne.shape();
+
+  fill(30)
+  stroke(255)
+  strokeWeight(1.5)
+  for (let piece = 0; piece < whitePieces.length; piece++) {
+    const element = whitePieces[piece];
+    whitePieces[piece].draw()
+  }
+  fill(255)
+  stroke(0)
+  for (let piece = 0; piece < blackPieces.length; piece++) {
+    const element = blackPieces[piece];
+    blackPieces[piece].draw()
+  }
+
   
 
 }
